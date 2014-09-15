84f91cc08bfa3e118d80400dc90d2c9ac5198b93
zejunzhang <eric900130@gmail.com>
2014-09-15 13:15:40 -0400

    First Commit

---
+++ src/com/structure/Point.java
@@ -0,0 +1,18 @@
+package com.structure;
+
+public class Point {
+	
+	int x;
+	int y;
+	
+	Point(){
+		x=0;
+		y=0;
+	}
+	
+	Point(int a, int b){
+		this.x=a;
+		this.y=b;
+	}
+
+}
---
+++ src/com/structure/PointGenerator.java
@@ -0,0 +1,47 @@
+package com.structure;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import com.structure.Point;
+
+public class PointGenerator {
+	
+	final int POINTNUM = 100;
+	
+	int pointsNum;
+	ArrayList points;
+	
+	public PointGenerator(){
+		this.pointsNum = POINTNUM;
+		this.points = new ArrayList();
+		
+		
+	}
+	
+	public PointGenerator(int num){
+		this.pointsNum = num;
+		this.points = new ArrayList();
+		
+		
+	}
+	
+	public void addPoints(){
+		Random rand = new Random();
+		int x = rand.nextInt(100);
+		int y = rand.nextInt(100);
+		
+		Point newPoint = new Point(x,y);
+		points.add(newPoint);
+	}
+	
+	public void addPoints(int x,int y){
+		Point newPoint = new Point(x,y);
+		points.add(newPoint);
+	}
+	
+	public ArrayList getPoints(){
+		return points;
+	}
+
+}
