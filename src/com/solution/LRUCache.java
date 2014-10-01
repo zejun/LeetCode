2ed2f13c05cc374a10a4b46434ebb422970fa851
Eric <eric900130@gmail.com>
2014-10-01 16:02:12 -0400

    LRUCache added

---
+++ src/com/solution/LRUCache.java
@@ -0,0 +1,17 @@
+package com.solution;
+
+public class LRUCache {
+	
+	public LRUCache(int capacity) {
+        
+    }
+    
+    public int get(int key) {
+        return 0;
+    }
+    
+    public void set(int key, int value) {
+        
+    }
+
+}
