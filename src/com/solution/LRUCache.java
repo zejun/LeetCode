d93b30ed6320b2944b9a4de6a5e9180918b2e093
Eric <eric900130@gmail.com>
2014-10-01 16:42:30 -0400

    with updated functions

--- src/com/solution/LRUCache.java
+++ src/com/solution/LRUCache.java
@@ -1,17 +1,37 @@
 package com.solution;
 
+import java.util.Map;
+import java.util.Stack;
+
 public class LRUCache {
 	
+	private int cacheCap;
+	private Stack<Map> cache;
+	
 	public LRUCache(int capacity) {
+        this.cacheCap = capacity;
+        this.cache = new Stack<Map>();
         
     }
     
     public int get(int key) {
-        return 0;
+    	int result = -1;
+    	
+    	for(int i=0;i<this.cacheCap;i++){
+    		
+    	}
+    	
+    	
+        return result;
     }
     
     public void set(int key, int value) {
         
+    	
+    	
     }
+    
 
 }
+
+
