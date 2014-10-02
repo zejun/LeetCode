26c2dd63b24f6ce0cd5c3f46c4355b1d60bd1f33
Eric <eric900130@gmail.com>
2014-10-02 11:11:53 -0400

    LRU cache structure updated with set method

--- src/com/solution/LRUCache.java
+++ src/com/solution/LRUCache.java
@@ -6,11 +6,11 @@ import java.util.Stack;
 public class LRUCache {
 	
 	private int cacheCap;
-	private Stack<Map> cache;
+	private Stack<DataPair> cache;
 	
 	public LRUCache(int capacity) {
         this.cacheCap = capacity;
-        this.cache = new Stack<Map>();
+        this.cache = new Stack<DataPair>();
         
     }
     
@@ -26,9 +26,52 @@ public class LRUCache {
     }
     
     public void set(int key, int value) {
-        
+        boolean exist = false;
+        Stack<DataPair> tem = new Stack<DataPair>();
     	
+    	for(int i=0;i<this.cacheCap;i++){
+    		DataPair check = cache.pop();
+    		//got the same key
+    		if(check.key==key){
+    			
+    			exist = true;
+    			break;
+    		}else{
+    			tem.push(check);
+    		}
+    	}
     	
+    	//check whether it exist
+    	if(exist){
+    		while(!tem.empty()){
+    			cache.push(tem.pop());
+    		}
+    		cache.push(new DataPair(key,value));
+    	}else{
+    		boolean isFirst = true;
+    		
+    		while(!tem.empty()){
+    			if(isFirst){
+    				tem.pop();
+    				isFirst=false;
+    			}else{
+    				cache.push(tem.pop());
+    			}
+    		}
+    		cache.push(new DataPair(key,value));
+    	}
+    	
+    }
+    
+    private class DataPair{
+    	public int key;
+    	public int val;
+    	
+    	public DataPair(int key,int val){
+    		this.key = key;
+    		this.val = val;
+    	}
+
     }
     
 
--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -13,40 +13,32 @@ public class Test {
 	public static void main(String args[]){
 		System.out.println("test");
 		
-//		Point pointa = new Point(3,1);
-//		Point pointb = new Point(12,3);
-//		Point pointc = new Point(3,1);
-//		Point pointd = new Point(-6,-1);
-//		Point[] input = {pointa,pointb,pointc,pointd};
-//		
-//		int result = MaxPointOnALine.maxPoint(input);
-//		
-//		System.out.println("The result is "+result);
-//		
-//		int test1 = 3;
-//		
-//		int v1 = test1/2;
-//		int v2 = test1-v1;
 		
-		ListNode a = new ListNode(2);
-		ListNode b = new ListNode(1);
-		ListNode c = new ListNode(4);
-		ListNode d = new ListNode(8);
-		a.next=b;
-		b.next=c;
-		c.next=d;
-		
-		//ListNode e = SortLinkedList.sortList(a);
-		ListNode e = InsertionSortLinkedList.insertionSort(a);
-		
-		
-		while(e!=null){
-			System.out.println(e.val);
-			e=e.next;
-		}
+		/**
+		 * Test sorted linked list
+		 */
+//		ListNode a = new ListNode(2);
+//		ListNode b = new ListNode(1);
+//		ListNode c = new ListNode(4);
+//		ListNode d = new ListNode(8);
+//		a.next=b;
+//		b.next=c;
+//		c.next=d;
+//		
+//		//ListNode e = SortLinkedList.sortList(a);
+//		ListNode e = InsertionSortLinkedList.insertionSort(a);
+//		
+//		
+//		while(e!=null){
+//			System.out.println(e.val);
+//			e=e.next;
+//		}
 		
 		//print linked list
 		
+		/**
+		 * test maximum number on the same line
+		 */
 		
 //		PointGenerator pg = new PointGenerator(3);
 //		pg.getPoints();	
