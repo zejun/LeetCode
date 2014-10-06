2913f870448a3795975d3940f856b68e031d1abd
Eric <eric900130@gmail.com>
2014-10-06 15:03:21 -0400

    test and traversal updated

--- src/com/solution/BinaryTreePostOrderTraversal.java
+++ src/com/solution/BinaryTreePostOrderTraversal.java
@@ -2,6 +2,7 @@ package com.solution;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import com.structure.TreeNode;
 
@@ -10,6 +11,7 @@ public class BinaryTreePostOrderTraversal {
 	public static List<Integer> postOrderTraversal(TreeNode root){
 		
 		List<Integer> result = new ArrayList<Integer>();
+		Stack<Integer> tem = new Stack<Integer>();
 		
 		for(int i=0;i<5;i++){
 			result.add(i);
--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -14,11 +14,21 @@ public class Test {
 		System.out.println("Test method");
 		
 		
+		
 		/**
 		 * test for binary tree post order traversal 
 		 */
-		TreeNode testInput = new TreeNode(5);
-		List<Integer> testInteger = BinaryTreePostOrderTraversal.postOrderTraversal(testInput);
+		TreeNode treeNode1 = new TreeNode(1);
+		TreeNode treeNode2 = new TreeNode(2);
+		TreeNode treeNode3 = new TreeNode(3);
+		TreeNode treeNode4 = new TreeNode(4);
+		TreeNode treeNode5 = new TreeNode(5);
+		
+		
+		
+		
+		
+		List<Integer> testInteger = BinaryTreePostOrderTraversal.postOrderTraversal(treeNode1);
 		
 		for(Integer result:testInteger){
 			
