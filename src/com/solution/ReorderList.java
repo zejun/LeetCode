6e73643f8eff053085a9f176d05167fcc43a5708
Eric <eric900130@gmail.com>
2014-10-07 18:47:58 -0400

    updated with reverse sort list

--- src/com/solution/ReorderList.java
+++ src/com/solution/ReorderList.java
@@ -7,5 +7,24 @@ public class ReorderList {
 	public static void reorderList(ListNode head){
 		
 	}
+	
+	public static ListNode listReverse(ListNode head){
+		
+		if(head==null||head.next==null){
+			return head;
+		}
+		
+		ListNode reverse_head = null;
+		
+		while(head!=null){
+			ListNode tem = head.next;
+			head.next = reverse_head;
+			reverse_head = head;
+			head = tem;
+		}
+		
+		return reverse_head;
+		
+	}
 
 }
--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -17,7 +17,22 @@ public class Test {
 		/**
 		 * test for reorder the list
 		 */
+		ListNode a = new ListNode(1);
+		ListNode b = new ListNode(2);
+		ListNode c = new ListNode(3);
+		ListNode d = new ListNode(4);
+		a.next=b;
+		b.next=c;
+		c.next=d;
 		
+		//ListNode e = SortLinkedList.sortList(a);
+		ListNode e = ReorderList.listReverse(a);
+		
+		
+		while(e!=null){
+			System.out.println(e.val);
+			e=e.next;
+		}
 		
 		
 		
