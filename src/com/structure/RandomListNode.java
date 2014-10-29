fad9849fcadadac8c04775eaadaee1dc4f369036
Eric <eric900130@gmail.com>
2014-10-29 13:25:18 -0400

    add random list node

---
+++ src/com/structure/RandomListNode.java
@@ -0,0 +1,12 @@
+package com.structure;
+
+public class RandomListNode {
+	
+	public int label;
+	public RandomListNode next,random;
+	
+	public RandomListNode(int value){
+		this.label = value;
+	}
+
+}
