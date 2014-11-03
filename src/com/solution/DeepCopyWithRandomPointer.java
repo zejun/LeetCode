e0926989bd6367ecc940bdc45af3843765f24494
Zejun <eric900130@gmail.com>
2014-11-02 23:06:52 -0500

    add tester classes for iterator test

--- src/com/solution/DeepCopyWithRandomPointer.java
+++ src/com/solution/DeepCopyWithRandomPointer.java
@@ -31,7 +31,7 @@ public class DeepCopyWithRandomPointer {
 		}
 		
 		
-		return null;
+		return dummy.next;
 		
 	}
 	
---
+++ src/com/structure/test/LinkedListTester.java
@@ -0,0 +1,22 @@
+package com.structure.test;
+
+public class LinkedListTester {
+
+	//default constructor for loop
+	public LinkedListTester(){
+		
+		
+		
+	}
+	
+	private void generator(){
+		
+		
+		
+	}
+	
+	private boolean tester(){
+		return true;
+	}
+	
+}
