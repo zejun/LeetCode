aed0064c3a27696f9f0ee93449ad2c29c3d5992d
Zejun <eric900130@gmail.com>
2014-11-02 23:03:24 -0500

    commit with dummy point

--- src/com/solution/DeepCopyWithRandomPointer.java
+++ src/com/solution/DeepCopyWithRandomPointer.java
@@ -13,7 +13,22 @@ public class DeepCopyWithRandomPointer {
 		
 		//first method copy with two iteration
 		//first iteration copy the regular add hash map to store all the point object
+		RandomListNode dummy = new RandomListNode(0);
+		RandomListNode tem = head;
+		RandomListNode pre;
 		
+		while(tem!=null){
+			//build up current item
+			RandomListNode current = tem;
+			
+			//test for first item
+			if(dummy.next==null){
+				pre = tem;
+				dummy.next = current;
+			}
+			
+			
+		}
 		
 		
 		return null;
