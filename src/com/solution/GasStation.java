1bc141d7b07472c67bf235a174ef133fdcf77149
Zejun <eric900130@gmail.com>
2014-11-10 16:14:09 -0500

    start to build solution

---
+++ src/com/solution/GasStation.java
@@ -0,0 +1,23 @@
+package com.solution;
+
+public class GasStation {
+	
+	public static int solutionI(int[] gas,int[] cost){
+		
+		//gas i station's gas
+		//cost i to i+1 cost
+		//time cost n^2
+		for(int i=0;i<gas.length;i++){
+			
+			int tank = 0;
+			
+			for(int j=0;j<=gas.length;j++){
+				
+			}
+			
+		}
+		
+		return -1;
+	}
+
+}
