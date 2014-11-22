f473a035b10c273e9ac6fa5ac0853fbafbf640ca
Zejun <eric900130@gmail.com>
2014-11-21 19:03:36 -0500

    return the reverse list

---
+++ src/com/solution/Reverse.java
@@ -0,0 +1,17 @@
+package com.solution;
+
+import com.structure.ListNode;
+
+public class Reverse {
+	
+	public static ListNode reverseLinkedList(ListNode head){
+		
+		ListNode dummy = new ListNode(0);
+		
+		
+		
+		return dummy.next;
+		
+	}
+
+}
